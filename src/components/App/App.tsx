061e94ffefc158485b428d7a47518f64ce896d96
A_Oliinyk <aolejnik916@gmail.com>
2025-06-11 18:46:23 +0200

    update movie gallery with pagination and ReactQuery

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,39 +8,57 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import type { MoviesHttpResponse } from "../../types/movie";
+import ReactPaginate from "react-paginate";
+import { useEffect } from "react";
 
 const notify = () => toast("No movies found for your request.");
 
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
   const [clickedMovie, setClickedMovie] = useState<Movie | null>(null);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
 
-  const handleSearch = async (data: string) => {
-    setIsLoading(true);
-    setError(false);
-    try {
-      const result = await fetchMovies(data);
-      if (result.length === 0) {
-        notify();
-      }
-      setMovies(result);
-    } catch {
-      setError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const { data, isLoading, isError } = useQuery<MoviesHttpResponse>({
+    queryKey: ["movies", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
+  });
+
+  const handleSearch = (searchQuery: string) => {
+    setQuery(searchQuery);
+    setPage(1);
   };
+
+  useEffect(() => {
+    if (data?.results.length === 0) {
+      notify();
+    }
+  }, [data]);
 
   return (
     <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
       {isLoading && <Loader />}
-      {error && <ErrorMessage />}
-      {!isLoading && movies.length > 0 && (
+      {isError && <ErrorMessage />}
+      {data && data.total_pages > 1 && (
+        <ReactPaginate
+          pageCount={data.total_pages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setPage(selected + 1)}
+          forcePage={page - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
+      {data && data.results.length > 0 && (
         <MovieGrid
-          movies={movies}
+          movies={data.results}
           onSelect={(movie) => {
             setClickedMovie(movie);
           }}
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,27 +1,23 @@
 import axios from "axios";
-import type { Movie } from "../types/movie";
-
-interface MoviesHttpResponse {
-  results: Movie[];
-}
+import type { MoviesHttpResponse } from "../types/movie";
 
 const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
-async function fetchMovies(query: string) {
+async function fetchMovies(query: string, page: number) {
   const response = await axios.get<MoviesHttpResponse>(BASE_URL, {
     params: {
       query: query,
       include_adult: false,
       language: "en-US",
-      page: 1,
+      page: page,
     },
     headers: {
       Authorization: `Bearer ${API_KEY}`,
     },
   });
   //   console.log(response.data.results);
-  return response.data.results;
+  return response.data;
 }
 
 export default fetchMovies;
